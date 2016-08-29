cd1553a06b83d7847183586da997876279a048e0
userk <balestrieri.gepp@gmail.com>
2016-08-29 21:48:16 +0200

    Identify feature in space

---
+++ filter/segmentation/CMakeFiles/extract_indices.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extract_indices.dir/segmenter.cpp.o"
+  "extract_indices.pdb"
+  "extract_indices"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/extract_indices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
