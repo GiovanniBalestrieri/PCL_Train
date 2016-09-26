da727d24798f771c3989fb72827a4e914fceb84e
userk <balestrieri.gepp@gmail.com>
2016-09-27 01:22:59 +0200

    Added support for kinect sensor

---
+++ kinect/grabber/CMakeFiles/grabber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grabber.dir/grabber.cpp.o"
+  "grabber.pdb"
+  "grabber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grabber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
